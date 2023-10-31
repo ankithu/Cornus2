47d5be1a30f67858ed1419f9ea583c5402a9e7a3
ankithu <ankith.udupa@gmail.com>
2023-10-31 14:33:34 -0400

    dash in vote-yes

--- Cornus/include/transactionHandlers/Participant.h
+++ Cornus/include/transactionHandlers/Participant.h
@@ -24,7 +24,7 @@ public:
         if(request.has_value()){
             if(request->type==RequestType::voteReq){
                 //log voteyes
-                send("VOTEYES");
+                send("VOTE-YES");
             }else{
                 abort();
             }
