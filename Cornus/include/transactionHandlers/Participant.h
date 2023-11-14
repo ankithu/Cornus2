d1fc99e8af31620c18231763cea18235d616a54b
Luke Lesh <lukelesh@Lukes-MacBook-Air-40.local>
2023-11-13 19:30:38 -0500

    moved decision log before commit

--- Cornus/include/transactionHandlers/Participant.h
+++ Cornus/include/transactionHandlers/Participant.h
@@ -3,68 +3,84 @@
 
 #include "TransactionHandler.hpp"
 /*
-TODO: 
+TODO:
 - Add logging
 */
 template <WorkerImpl WorkerT>
 class Participant : public TransactionHandler
 {
 public:
-    Participant(TransactionConfig &config, HostID hostname, HostConfig& hostConfig) : TransactionHandler(config,hostname, hostConfig), worker() {
+    Participant(TransactionConfig &config, HostID hostname, HostConfig &hostConfig) : TransactionHandler(config, hostname, hostConfig), worker()
+    {
     }
-    virtual Decision handleTransaction(const Request& start_request) override
+    virtual Decision handleTransaction(const Request &start_request) override
     {
 
-        if (!votedYes(start_request)){
+        if (!votedYes(start_request))
+        {
             return "ABORT";
         }
 
-        //voted yes
+        // voted yes
         std::cout << "voting yes as participant. " << std::endl;
         std::string resp = RequestInterface::LOG_ONCE("VOTEYES", this->config.txid, this->hostname, LogType::TRANSACTION);
         std::cout << "DBMS RESPONSE OF " << resp << std::endl;
-        if(resp=="VOTEYES"){
+        if (resp == "VOTEYES")
+        {
             std::cout << "sending vote yes back to coordinator!" << std::endl;
             send("VOTEYES");
             Decision decision;
-            //Execution phase
+            // Execution phase
             auto commit_request = this->messages.waitForNextMessageWithTimeout(this->hostConfig.timeout);
-            if(commit_request.has_value()){
-                if(commit_request->type==RequestType::Commit){
-                    decision="COMMIT";
-                }else if(commit_request->type==RequestType::Abort){
-                    decision="ABORT";
-                }else{
-                    //TODO: message at wrong time
+            if (commit_request.has_value())
+            {
+                if (commit_request->type == RequestType::Commit)
+                {
+                    decision = "COMMIT";
                 }
-            } else{
+                else if (commit_request->type == RequestType::Abort)
+                {
+                    decision = "ABORT";
+                }
+                else
+                {
+                    // TODO: message at wrong time
+                }
+            }
+            else
+            {
                 decision = this->terminationProtocol();
             }
-            if (decision == "COMMIT"){
+            RequestInterface::LOG_WRITE(decision, this->config.txid, this->hostname, LogType::TRANSACTION);
+            if (decision == "COMMIT")
+            {
                 worker.COMMIT(start_request.req.body);
             }
-            RequestInterface::LOG_WRITE(decision, this->config.txid, this->hostname, LogType::TRANSACTION);
             return decision;
-        }else{
+        }
+        else
+        {
             std::cout << "aborting since DBMS response was not vote yes!" << std::endl;
             send("ABORT");
             return "ABORT";
         }
     }
-   
-    void send(std::string type){
-        std::string path="/"+type+"/"+std::to_string(this->config.txid);
+
+    void send(std::string type)
+    {
+        std::string path = "/" + type + "/" + std::to_string(this->config.txid);
         httplib::Params params;
         params.emplace("request", "");
         params.emplace("type", type);
         params.emplace("sender", this->hostname);
-        RequestInterface::SEND_RPC(this->config.coordinator,path,params);
+        RequestInterface::SEND_RPC(this->config.coordinator, path, params);
     }
 
 private:
     WorkerT worker;
 
-    inline bool votedYes(const Request& request) {
+    inline bool votedYes(const Request &request)
+    {
         return worker.VOTE_REQ(request.req.body);
     }
 };
