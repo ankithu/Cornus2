ad3c571d292b17927e7be7bee8fb45207a7f177a
Luke Lesh <lukelesh@Lukes-MacBook-Air-40.local>
2023-11-17 09:33:18 -0500

    Completed the replicator and global message handler

--- Cornus/include/messaging/GlobalMessageHandler.hpp
+++ Cornus/include/messaging/GlobalMessageHandler.hpp
@@ -13,6 +13,10 @@
 #include "../transactionHandlers/Participant2.h"
 #include "../transactionHandlers/Replicator.h"
 
+using TransactionHandler = NewTransactionHandler;
+using Coordinator = NewCoordinator;
+using Participant = NewParticipant<WorkerT>;
+
 // onRequest function, in class or in global namespace, takes HTTP request object
 
 class GlobalMessageHandler
@@ -22,24 +26,22 @@ public:
     {
         httplib::Server::Handler handler;
         svr.Post("/TRANSACTION", [&](const httplib::Request &req, httplib::Response &res)
-                 { onClientRequest(req, res); });
+                 { onClientRequest(req, res, &handlers); });
         // handler for below endpoints launches another thread that is independent so ACK response should happen immediately after thread launch
         svr.Post("/VOTEREQ/:txid", [&](const httplib::Request &req, httplib::Response &res)
-                 { std::thread process(&GlobalMessageHandler::onNewRequest<NewParticipant<WorkerT>>, this, req, RequestType::voteReq); process.detach(); });
+                 { std::thread process(&GlobalMessageHandler::onNewRequest<Participant>, this, req, RequestType::voteReq, &handlers); process.detach(); });
         svr.Post("/VOTEYES/:txid", [&](const httplib::Request &req, httplib::Response &res)
-                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::voteYes); process.detach(); });
+                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::voteYes, &handlers); process.detach(); });
         svr.Post("/ABORT/:txid", [&](const httplib::Request &req, httplib::Response &res)
-                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::Abort); process.detach(); });
+                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::Abort, &handlers); process.detach(); });
         svr.Post("/COMMIT/:txid", [&](const httplib::Request &req, httplib::Response &res)
-                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::Commit); process.detach(); });
-        /*
+                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::Commit, &handlers); process.detach(); });
         svr.Post("/WILLCOMMIT/:txid", [&](const httplib::Request &req, httplib::Response &res)
-                 { std::thread process(&GlobalMessageHandler::onNewRequest<Replicator>, this, req, RequestType::willCommit); process.detach(); });
+                 { std::thread process(&GlobalMessageHandler::onNewRequest<Replicator>, this, req, RequestType::willCommit, &replicators); process.detach(); });
         svr.Post("/WILLABORT/:txid", [&](const httplib::Request &req, httplib::Response &res)
-                 { std::thread process(&GlobalMessageHandler::onNewRequest<Replicator>, this, req, RequestType::willAbort); process.detach(); });
+                 { std::thread process(&GlobalMessageHandler::onNewRequest<Replicator>, this, req, RequestType::willAbort, &replicators); process.detach(); });
         svr.Post("/DECISIONCOMPLETED/:txid", [&](const httplib::Request &req, httplib::Response &res)
-                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::decisionCompleted);process.detach(); });
-        */
+                 { std::thread process(&GlobalMessageHandler::onOldRequest, this, req, RequestType::decisionCompleted, &replicators);process.detach(); });
         this->hostname = hostConfig.id;
         std::cout << "Starting server..." << hostConfig.host << " " << hostConfig.port << std::endl;
         if (!svr.listen(hostConfig.host, hostConfig.port))
@@ -49,35 +51,35 @@ public:
     }
 
     template <class Node>
-    void onNewRequest(const httplib::Request req, RequestType type)
+    void onNewRequest(const httplib::Request req, RequestType type, std::map<TransactionId, TransactionHandler *> *transactions)
     {
         std::cout << "Got new requet as a participant" << std::endl;
         TransactionId txid = getTxId(req);
         std::cout << "txid: " << txid << std::endl;
         Request request = Request(type, txid, req);
-        Node *n = createNode<Node>(request, txid);
+        Node *n = createNode<Node>(request, txid, transactions);
         n->handleTransaction(request);
-        removeFromMap(txid);
+        removeFromMap(txid, transactions);
     }
 
-    void onClientRequest(const httplib::Request &req, httplib::Response &res)
+    void onClientRequest(const httplib::Request &req, httplib::Response &res, std::map<TransactionId, TransactionHandler *> *transactions)
     {
         std::cout << "new client request" << std::endl;
         TransactionId txid = getUniqueTransactionId();
         std::cout << "Created transaction id: " << txid << std::endl;
         Request request = Request(RequestType::transaction, txid, req);
-        NewCoordinator *n = createNode<NewCoordinator>(request, txid);
+        Coordinator *n = createNode<Coordinator>(request, txid, transactions);
         Decision d = n->handleTransaction(request);
         res.set_content(d, "text/plain");
-        removeFromMap(txid);
+        removeFromMap(txid, transactions);
     }
 
-    void onOldRequest(const httplib::Request req, RequestType type)
+    void onOldRequest(const httplib::Request req, RequestType type, std::map<TransactionId, TransactionHandler *> *transactions)
     {
 
         TransactionId txid = getTxId(req);
         Request request = Request(type, txid, req);
-        sendMessage(txid, request);
+        sendMessage(txid, request, transactions);
     }
 
 private:
@@ -97,32 +99,32 @@ private:
     }
 
     template <class Node>
-    Node *createNode(Request request, TransactionId txid)
+    Node *createNode(Request request, TransactionId txid, std::map<TransactionId, TransactionHandler *> *transactions)
     {
         std::unique_lock lockMutex(mapMutex);
         TransactionConfig conf(request.getParam("config"), txid);
         Node *p = new Node(conf, hostname, hostConfig);
-        transactions[txid] = p;
+        (*transactions)[txid] = p;
         return p;
     }
 
-    void removeFromMap(TransactionId txid)
+    void removeFromMap(TransactionId txid, std::map<TransactionId, TransactionHandler *> *transactions)
     {
         std::unique_lock lockMutex(mapMutex);
-        if (transactions.find(txid) != transactions.end())
+        if (transactions->find(txid) != transactions->end())
         {
-            NewTransactionHandler *n = transactions[txid];
+            TransactionHandler *n = (*transactions)[txid];
             delete n;
-            transactions.erase(txid);
+            transactions->erase(txid);
         }
     }
 
-    void sendMessage(TransactionId txid, Request request)
+    void sendMessage(TransactionId txid, Request request, std::map<TransactionId, TransactionHandler *> *transactions)
     {
         std::unique_lock lockMutex(mapMutex);
-        if (transactions.find(txid) != transactions.end())
+        if (transactions->find(txid) != transactions->end())
         {
-            transactions[txid]->handle(request);
+            (*transactions)[txid]->handle(request);
         }
     }
 
@@ -134,8 +136,8 @@ private:
     u_int16_t nodeId;
     u_int32_t transactionCounter = 0;
     std::mutex mapMutex;
-    std::map<TransactionId, NewTransactionHandler *> transactions;
-    std::map<TransactionId, NewTransactionHandler *> replicators;
+    std::map<TransactionId, TransactionHandler *> handlers;
+    std::map<TransactionId, TransactionHandler *> replicators;
     HostID hostname;
 };
 
--- Cornus/include/transactionHandlers/Replicator.h
+++ Cornus/include/transactionHandlers/Replicator.h
@@ -12,7 +12,14 @@ public:
         auto completed = this->messages.waitForNextMessageWithTimeout(this->hostConfig.timeout);
         if (!(completed.has_value()) || !(completed->type == RequestType::decisionCompleted))
         {
-            logFinal("TODO", this->config.txid, request.getParam("hostname"));
+            if (request.type == RequestType::willAbort)
+            {
+                logFinal("ABORT", this->config.txid, request.getParam("hostname"));
+            }
+            else if (request.type == RequestType::willCommit)
+            {
+                logFinal("COMMIT", this->config.txid, request.getParam("hostname"));
+            }
         }
         return "";
     }
