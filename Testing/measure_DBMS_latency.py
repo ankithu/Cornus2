a5faaf48908e0b2dfd9630cd5292d5885f4ff4b7
Luke Lesh <lukelesh@Lukes-MacBook-Air-40.local>
2023-11-30 19:22:05 -0500

    added DBMS expection latency

--- Testing/measure_DBMS_latency.py
+++ Testing/measure_DBMS_latency.py
@@ -1,21 +1,51 @@
+"""
+running the script:
+python3 measure_DBMS_latency.py localhost:9000
+
+"""
+
 import time
 import argparse
 import requests
+import concurrent.futures
+
+def make_request(url):
+    headers = {
+    'content-type': 'text/plain',
+    }
+    start = time.time()
+    failed = False
+    try:
+        response = requests.post(url, data="COMMIT", headers=headers)
+    except: 
+        failed = True
+        print("CONNECTION REFUSED: ", url)
+    end = time.time()
+    latency = end - start
+    return (latency, failed)
+
+def send_requests_concurrent(transactions):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+
+        for transaction in transactions:
+            futures.append(executor.submit(make_request, url=transaction))
+
+        total = 0
+        n = 0
+        for future in concurrent.futures.as_completed(futures):
+            if not future.result()[1]:
+                total = total + future.result()[0]
+                n = n + 1
+        return total / n
 
 def benchmark(dbmshostport):
-    total = 0
-    n = 1000
+    n = 6
+    transactions = []
     for i in range(n):
-        url = "http://" + dbmshostport + "/LOG_WRITE/0/0/0"
-        headers = {
-        'content-type': 'text/plain',
-        }
-        start = time.time()
-        response = requests.post(url, data="COMMIT", headers=headers)
-        end = time.time()
-        latency = end - start
-        total = total + latency
-    average = total / n
+        url = "http://localhost:9000/LOG_WRITE/" + str(i) + "/1/TRANSACTION"
+        transactions.append(url)
+    average = send_requests_concurrent(transactions)
     print("Average: ", average)
 
 
